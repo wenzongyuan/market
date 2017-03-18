c85e2fad4bdc693ae1b23a2cb84f5d7edb0ca5bc
wenzongyuan <18817808952@163.com>
2017-03-18 18:06:12 +0800

    add package

---
+++ member/src/main/java/com/wzymarket/member/remote/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author asus
+ *
+ */
+package com.wzymarket.member.remote;
---
+++ member/src/main/java/com/wzymarket/memeber/handler/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author asus
+ *
+ */
+package com.wzymarket.memeber.handler.impl;
